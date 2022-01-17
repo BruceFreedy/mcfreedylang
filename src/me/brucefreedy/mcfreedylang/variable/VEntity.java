c75b978b124dcb88cf1874ae98be541008fc2fa1
Bruce0203 <BruceFreedy@gmail.com>
2022-01-17 19:40:58 +0900

    commit

--- src/me/brucefreedy/mcfreedylang/variable/VEntity.java
+++ src/me/brucefreedy/mcfreedylang/variable/VEntity.java
@@ -3,12 +3,16 @@ package me.brucefreedy.mcfreedylang.variable;
 import me.brucefreedy.freedylang.lang.variable.AbstractVar;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 public class VEntity<T extends Entity> extends AbstractVar<T> {
     
     public VEntity(T object) {
         super(object);
-        register("location", method(o -> o instanceof VLocation, Location.class, object::teleport, object::getLocation));
+        register("location", method(o -> o instanceof VLocation, Location.class, location -> {
+            object.sendMessage("debugmessage");
+            object.teleport(location);
+        }, object::getLocation));
         register("name", stringValue(object::setCustomName, object::getName));
     }
 
