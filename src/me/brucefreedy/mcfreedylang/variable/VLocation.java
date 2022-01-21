c6bb7fd2bc4e2dab5ca64cc68ef06999840e7a48
Bruce0203 <BruceFreedy@gmail.com>
2022-01-21 20:41:52 +0900

    commit

--- src/me/brucefreedy/mcfreedylang/variable/VLocation.java
+++ src/me/brucefreedy/mcfreedylang/variable/VLocation.java
@@ -17,4 +17,9 @@ public class VLocation extends AbstractVar<Location> {
         register("block", (Method) (unit, params) -> new VBlock(object.getBlock().getState()));
     }
 
+    @Override
+    public String toString() {
+        return object.toString();
+    }
+
 }
