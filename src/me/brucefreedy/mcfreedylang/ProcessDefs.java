731633a869d7fac6eb165694f88d3ebb059df64b
Bruce0203 <BruceFreedy@gmail.com>
2022-01-21 16:35:35 +0900

    commit

--- src/me/brucefreedy/mcfreedylang/ProcessDefs.java
+++ src/me/brucefreedy/mcfreedylang/ProcessDefs.java
@@ -3,10 +3,7 @@ package me.brucefreedy.mcfreedylang;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import me.brucefreedy.freedylang.lang.Process;
-import me.brucefreedy.mcfreedylang.process.Async;
-import me.brucefreedy.mcfreedylang.process.CancelTask;
-import me.brucefreedy.mcfreedylang.process.DelayTask;
-import me.brucefreedy.mcfreedylang.process.RepeatTask;
+import me.brucefreedy.mcfreedylang.process.*;
 import me.brucefreedy.mcfreedylang.process.event.EventListener;
 
 import java.util.function.Supplier;
@@ -27,6 +24,7 @@ public enum ProcessDefs {
     INVENTORY_CLICK(EventListener.InventoryClick::new),
     INVENTORY_DRAG(EventListener.InventoryDrag::new),
     INVENTORY_CLOSE(EventListener.InventoryClose::new),
+    MATERIAL(MaterialRef::new),
     ;
     private final Supplier<Process<?>> supplier;
 }
