54c347c4af79a7074a01de5c3e85b9315a7fce9c
Bruce0203 <BruceFreedy@gmail.com>
2022-01-30 09:17:38 +0900

    commit

--- src/me/brucefreedy/mcfreedylang/variable/enumvar/VMaterial.java
+++ src/me/brucefreedy/mcfreedylang/variable/enumvar/VMaterial.java
@@ -1,20 +1,29 @@
 package me.brucefreedy.mcfreedylang.variable.enumvar;
 
+import me.brucefreedy.common.List;
+import me.brucefreedy.freedylang.lang.abst.Method;
+import me.brucefreedy.freedylang.lang.variable.bool.Bool;
 import org.bukkit.Material;
+
+import java.util.function.Function;
 
 public class VMaterial extends AbstractEnumVar<Material> {
     public VMaterial(Material m) {
         super(m);
-        register("isSolid", func(o -> object.isSolid()));
-        register("hasGravity", func(o -> object.hasGravity()));
-        register("isBlock", func(o -> object.isBlock()));
-        register("isItem", func(o -> object.isItem()));
-        register("isBurnable", func(o -> object.isBurnable()));
-        register("isEdible", func(o -> object.isEdible()));
-        register("isFlammable", func(o -> object.isFlammable()));
-        register("isFuel", func(o -> object.isFuel()));
-        register("isInteractable", func(o -> object.isInteractable()));
-        register("isOccluding", func(o -> object.isOccluding()));
-        register("isRecord", func(o -> object.isRecord()));
+        register("isSolid", bool(o -> object.isSolid()));
+        register("hasGravity", bool(o -> object.hasGravity()));
+        register("isBlock", bool(o -> object.isBlock()));
+        register("isItem", bool(o -> object.isItem()));
+        register("isBurnable", bool(o -> object.isBurnable()));
+        register("isEdible", bool(o -> object.isEdible()));
+        register("isFlammable", bool(o -> object.isFlammable()));
+        register("isFuel", bool(o -> object.isFuel()));
+        register("isInteractable", bool(o -> object.isInteractable()));
+        register("isOccluding", bool(o -> object.isOccluding()));
+        register("isRecord", bool(o -> object.isRecord()));
+    }
+
+    protected Method bool(Function<List<?>, Boolean> function) {
+        return super.func(o -> Bool.get(function.apply(o)));
     }
 }
